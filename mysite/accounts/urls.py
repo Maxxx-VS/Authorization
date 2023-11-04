443436779eaf8d2a96f51fae665cd7e45ffb084e
Высочанский Максим <126272688+maxxx-vs@users.noreply.github.com>
2023-11-04 19:35:37 +0600

    Django with Authorization #2

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, include, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('news', views.news, name='news'),
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
+menu = [
+        {'title': "Новости", 'url_name': 'news'},
+        {'title': "О компании", 'url_name': 'about'},
+        {'title': "Контакты", 'url_name': 'contacts'},
+        {'title': "Регистрация на сайте", 'url_name': 'register'},
+        {'title': "Войти на сайт", 'url_name': 'login'},
+]
+data_db = [
+    {'id': 1, 'title': 'Анкерно-угловая опора', 'content': 'Описание А-УО', 'is_published': True},
+    {'id': 2, 'title': 'Промежуточная опора', 'content': 'Описание ПО', 'is_published': True},
+    {'id': 3, 'title': 'Концевая опора', 'content': 'Описание КО', 'is_published': True},
+]
+def index(request):
+    data = {
+        'title': "Главная страница",
+        'menu': menu,
+        'post': data_db,
+    }
+    return render(request, 'index.html', context=data)
+def news(request):
+    data = {'title': "Новости"}
+    return render(request, 'news.html', data)
+def about(request):
+    return render(request, 'about.html', {'title': 'О компании'})
+def contacts(request):
+    return HttpResponse("<h1>Контакты</h1")
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 def register(request):
     if request.method == 'POST':
