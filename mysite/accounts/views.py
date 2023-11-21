c4fca9c78b475b2a9dae448369b95a2f56dada62
Высочанский Максим <126272688+maxxx-vs@users.noreply.github.com>
2023-11-21 20:24:45 +0600

    Django with Authorization #16 Добавлена страница обработки фотографии

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -19,6 +19,7 @@ menu = [
         {'title': "Блог автора", 'url_name': 'blog'},
         {'title': "Регистрация на сайте", 'url_name': 'register'},
         {'title': "Войти на сайт", 'url_name': 'login'},
+        {'title': "Обработка изображений", 'url_name': 'process_image'},
 ]
 data_db = [
     {'id': 1, 'title': 'Анкерно-угловая опора', 'content': 'Описание А-УО', 'is_published': True},
@@ -164,6 +165,41 @@ def DarkFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     for color in (r, g, b):
         result = [int(r/3), int(g/3), int(b/3)]
     return tuple(result)
+def BrightFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
+    result = []
+    for color in (r, g, b):
+        result = [int(r*3), int(g*3), int(b*3)]
+    return tuple(result)
+
+def RedFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
+    result = []
+    for color in (r, g, b):
+        result = [int(r*3), int(g*1), int(b*1)]
+    return tuple(result)
+
+def GreenFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
+    result = []
+    for color in (r, g, b):
+        result = [int(r*1), int(g*3), int(b*1)]
+    return tuple(result)
+
+def BlueFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
+    result = []
+    for color in (r, g, b):
+        result = [int(r*1), int(g*1), int(b*3)]
+    return tuple(result)
+
+def Drugs_eye(r: int, g: int, b: int) -> tuple[int, int, int]:
+    result = []
+    if r == 90 and g == 141 and b == 61:
+        result = [int(randrange(25, 250)), int(randrange(25, 250)), int(randrange(25, 250))]
+    elif r == 165 and g ==94 and b ==60:
+        result = [int(250), int(20), int(5)]
+    elif r ==74 and g ==114 and b == 52:
+        result = [int(randrange(25, 250)), int(randrange(25, 250)), int(randrange(25, 250))]
+    else:
+        result = [r,g, b]
+    return tuple(result)
 def apply_filter(img: Image.Image, filt) -> Image.Image:
     for i in range(img.width):
         for j in range(img.height):
@@ -175,8 +211,9 @@ def process_image(request):
     if request.method == "POST":
         image_file = request.FILES['image']
         image = Image.open(image_file)
-        filtered_image = apply_filter(image, DarkFilter)
+        filtered_image = apply_filter(image, GreenFilter)
         filtered_image.save('processed_image.jpg')
+        filtered_image.show()
         return HttpResponse('Изображение обработанао и сохранено')
     return render(request, 'upload_image.html')
 
